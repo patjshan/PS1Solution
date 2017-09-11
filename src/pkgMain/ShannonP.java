12e270270074564cd9ad33c60799d2de43c36230
PatrickShannon <PatrickShannon@patricks-mbp.fios-router.home>
2017-09-10 21:32:23 -0400

    added github account comment and Hello world method

---
+++ src/pkgMain/ShannonP.java
@@ -0,0 +1,10 @@
+package pkgMain;
+
+public class ShannonP {
+	public static void shannonp(){
+		
+	String HW = ("Hello World");
+	System.out.print(HW);
+	}
+
+}
--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -3,9 +3,11 @@ package pkgMain;
 public class cMain {
 
 	public static void main(String[] args) {
-
+		 
+		// GitHub account: patjshan
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
+	
+	ShannonP.shannonp();
 	}
-
 }
